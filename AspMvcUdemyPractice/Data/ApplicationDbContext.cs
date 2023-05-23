ac1c7d06332da6f02260b6b848c992a58a0da478
ReiieL <asura.mier@gmail.com>
2023-05-23 12:37:43 +0800

    Add project files.

---
+++ AspMvcUdemyPractice/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspMvcUdemyPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspMvcUdemyPractice.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ AspMvcUdemyPractice/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspMvcUdemyPractice.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
